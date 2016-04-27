b8854f071ff48a30ea61a9abaf9f1526baee15f3
Ner'zhul <nerzhul@users.noreply.github.com>
2016-04-27 12:34:40 +0200

    Update atomicbool.go

--- atomics/atomicbool.go
+++ atomics/atomicbool.go
@@ -2,15 +2,15 @@
 
 import "sync/atomic"
 
-type TAtomBool struct {flag int32}
+type AtomicBool struct {flag int32}
 
-func (b *TAtomBool) Set(value bool) {
+func (b *AtomicBool) Set(value bool) {
   var i int32 = 0
   if value {i = 1}
   atomic.StoreInt32(&(b.flag), int32(i))
 }
 
-func (b *TAtomBool) Get() bool {
+func (b *AtomicBool) Get() bool {
   if atomic.LoadInt32(&(b.flag)) != 0 {return true}
   return false
 }
